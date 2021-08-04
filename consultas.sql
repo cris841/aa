361a6352583d674e16231051e3b8f1c2aae7362c
cris841 <83967055+cris841@users.noreply.github.com>
2021-08-04 09:38:04 -0500

    Add files via upload

---
+++ consultas.sql
@@ -0,0 +1,17 @@
+insert into Propietario(nom_propietario,apel_propietario)
+	values ('juan','smit'),('stefanny,vargas')
+
+insert into Apartamento(bloque,num_apartamento)
+	values ('2','a500'),('3','b99')
+
+insert into  Residente(id_residente,nombre_residente)
+	values ('1036794561','Alberto'),('6874556','Mina')		
+
+insert into Mascota(especie	raza)
+	values ('perro','bull dog'),('perro','b99')	
+
+insert into Veiculo(Marca,Modelo)
+	values ('bmw','x6'),('Alfa Romeo','Veloce')
+
+insert into Parqueadero(Número	propietario)
+	values ('h12','stefanny'),('b8','juan')		
